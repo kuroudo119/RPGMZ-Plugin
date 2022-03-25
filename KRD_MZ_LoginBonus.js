98f5ca4b4a2eb7b4bb3968c9ac6c39c7dd3f573f
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2022-03-26 04:55:51 +0900

    時刻リセット式ログインボーナスを追加。

--- KRD_MZ_LoginBonus.js
+++ KRD_MZ_LoginBonus.js
@@ -1,33 +1,62 @@
 /*:
  * @target MZ
  * @plugindesc ログインボーナス支援
- * @author kuroudo119 (くろうど)
+ * @url https://twitter.com/kuroudo119/
  * @url https://github.com/kuroudo119/RPGMZ-Plugin
+ * @author kuroudo119 (くろうど)
  * 
  * @command KRD_canGetBonus
- * @text ボーナス受取可否
+ * @text タイマー式ボーナス
  * @desc タイマーの時間経過を確認し、経過している場合スイッチがONになります。
- ***
+ * 
  * @arg name
  * @text タイマー名
  * @desc 複数タイマーを使い分ける場合の名前です。
  * @default bonus
  * @type string
- ***
+ * 
  * @arg borderSecond
  * @text 経過秒数
- * @desc ログインボーナス発生に必要な秒数です。1時間は3600秒。24時間は86400秒です。
+ * @desc ボーナス取得に必要な秒数です。1時間は3600秒。24時間は86400秒です。
  * @default 43200
  * @type number
- ***
+ * 
  * @arg swBonus
  * @text ボーナススイッチ
- * @desc ボーナス発生可否を入れるスイッチです。共用できます。
+ * @desc ボーナス取得可否が入るスイッチです。共用できます。
+ * @default 1
+ * @type switch
+ * 
+ * @command KRD_isNewDate
+ * @text 時刻リセット式ボーナス
+ * @desc 時刻リセット式ボーナスです。その日のボーナスを未取得の場合にスイッチがONになります。
+ * 
+ * @arg resetHour
+ * @text リセット時
+ * @desc リセット時刻の「時」です。この時刻を過ぎるとその日のボーナスを受け取れます。
+ * @default 0
+ * @type number
+ * @min 0
+ * @max 23
+ * 
+ * @arg resetMinutes
+ * @text リセット分
+ * @desc リセット時刻の「分」です。
+ * @default 0
+ * @type number
+ * @min 0
+ * @max 59
+ * 
+ * @arg swBonus
+ * @text ボーナススイッチ
+ * @desc ボーナス取得可否が入るスイッチです。
  * @default 1
  * @type switch
  * 
  * @help
 # KRD_MZ_LoginBonus.js
+
+ログインボーナス支援
 
 ## 権利表記
 
@@ -42,6 +71,8 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 
 - ver.0.0.1 (2021/06/01) 非公開版完成
 - ver.1.0.0 (2021/06/01) 公開開始
+- ver.1.0.1 (2021/06/18) プラグインコマンド引数のtypeを修正
+- ver.1.1.0 (2022/03/26) 時刻リセット式を追加
 
  * 
  * 
@@ -52,7 +83,7 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 
 "use strict";
 
-const PLUGIN_NAME	= document.currentScript.src.match(/^.*\/(.*).js$/)[1];
+const PLUGIN_NAME = document.currentScript.src.match(/^.*\/(.*).js$/)[1];
 
 //------------------------------------------------
 // プラグインコマンド
@@ -64,7 +95,15 @@ PluginManager.registerCommand(PLUGIN_NAME, "KRD_canGetBonus", args => {
 	$gameSwitches.setValue(swBonus, $gameSystem.canGetBonus(border, name));
 });
 
+PluginManager.registerCommand(PLUGIN_NAME, "KRD_isNewDate", args => {
+	const resetHour = Number(args.resetHour) || 0;
+	const resetMinutes = Number(args.resetMinutes) || 0;
+	const swBonus = Number(args.swBonus) || 0;
+	$gameSwitches.setValue(swBonus, $gameSystem.isNewDate(resetHour, resetMinutes));
+});
+
 //------------------------------------------------
+// タイマー式
 
 Game_System.prototype.canGetBonus = function(second, name = "bonus") {
 	this._oldTime = this._oldTime || {};
@@ -80,4 +119,23 @@ Game_System.prototype.canGetBonus = function(second, name = "bonus") {
 	}
 };
 
+//------------------------------------------------
+// 特定時刻リセット式
+
+Game_System.prototype.isNewDate = function(resetHour = 0, resetMinutes = 0) {
+	this._last = this._last || 0;
+	const now = new Date();
+	const today = (new Date(now.getFullYear(), now.getMonth(), now.getDate(), resetHour, resetMinutes)).getTime();
+	const tomorrow = (new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, resetHour, resetMinutes)).getTime();
+	const border = now.getTime() >= today ? tomorrow : today;
+
+	if (this._last < border) {
+		this._last = border;
+		return true;
+	} else {
+		return false;
+	}
+};
+
+//------------------------------------------------
 })();

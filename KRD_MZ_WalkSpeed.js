c0e0a335a34f6ae81ef4abba8ea699e38c261a11
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2022-10-07 21:36:08 +0900

    乗り物に対応

--- KRD_MZ_WalkSpeed.js
+++ KRD_MZ_WalkSpeed.js
@@ -11,6 +11,10 @@
  * @default false
  * @type boolean
  * 
+ * @command setMoveSpeed
+ * @text 移動スピード設定
+ * @desc 移動スピード設定処理を実行します。移動ルートの設定コマンド後などに使用してください。
+ * 
  * @help
 # KRD_MZ_WalkSpeed.js
 
@@ -30,6 +34,7 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 - ver.0.0.1 (2022/10/06) 作成開始
 - ver.0.1.0 (2022/10/06) 非公開版完成
 - ver.1.0.0 (2022/10/06) 公開
+- ver.1.1.0 (2022/10/07) 乗り物などに対応した。
 
 ## 機能概要
 
@@ -37,9 +42,20 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 
 ダッシュ時のスピードは +1 ですが、最大でも 5 となります。
 
+### プラグインパラメータ「職業メモ欄使用」
+
 プラグインパラメータ「職業メモ欄使用」が true の時、
+タグ <moveSpeed:5> が使用可能になります。
+
 職業のメモ欄に <moveSpeed:5> と記述した場合、
 そのアクターが先頭の場合、歩行スピードが 5 となります。
+
+タグが無い場合の歩行スピードは 4 になります。
+
+### プラグインコマンド「移動スピード設定」
+
+移動スピード設定処理を実行します。
+移動ルートの設定コマンド後などに使用してください。
 
  * 
  * 
@@ -59,12 +75,26 @@ const NEW_WALK_SPEED = 4.5;
 const MAX_DASH_SPEED = 5;
 
 //--------------------------------------
+// プラグインコマンド
 
+PluginManager.registerCommand(PLUGIN_NAME, "setMoveSpeed", args => {
+	$gamePlayer.setMoveSpeed($gamePlayer._moveSpeed);
+});
+
+//--------------------------------------
+
+const KRD_Game_Player_setMoveSpeed = Game_Player.prototype.setMoveSpeed;
 Game_Player.prototype.setMoveSpeed = function(moveSpeed) {
-	if (USE_CLASS_META) {
-		this._moveSpeed = this.leaderMoveSpeed();
+	if (this.isMoveRouteForcing()) {
+		KRD_Game_Player_setMoveSpeed.apply(this, arguments);
+	} else if (this._vehicleType === "walk" || this._vehicleGettingOff) {
+		if (USE_CLASS_META) {
+			this._moveSpeed = this.leaderMoveSpeed();
+		} else {
+			this._moveSpeed = NEW_WALK_SPEED;
+		}
 	} else {
-		this._moveSpeed = NEW_WALK_SPEED;
+		KRD_Game_Player_setMoveSpeed.apply(this, arguments);
 	}
 };
 
@@ -80,24 +110,28 @@ Game_Player.prototype.leaderMoveSpeed = function() {
 	return Number($dataClasses[$gameParty.leader()._classId]?.meta.moveSpeed) || DEFAULT_WALK_SPEED;
 };
 
-const KRD_Scene_Menu_terminate = Scene_Menu.prototype.terminate;
-Scene_Menu.prototype.terminate = function() {
-	KRD_Scene_Menu_terminate.apply(this, arguments);
-	if (USE_CLASS_META) {
-		$gamePlayer.setMoveSpeed(DEFAULT_WALK_SPEED);
-	}
+const KRD_Scene_Map_start = Scene_Map.prototype.start;
+Scene_Map.prototype.start = function() {
+	KRD_Scene_Map_start.apply(this, arguments);
+	$gamePlayer.setMoveSpeed($gamePlayer._moveSpeed);
 };
 
 const KRD_Game_Party_addActor = Game_Party.prototype.addActor;
 Game_Party.prototype.addActor = function(actorId) {
 	KRD_Game_Party_addActor.apply(this, arguments);
-	$gamePlayer.setMoveSpeed(DEFAULT_WALK_SPEED);
+	$gamePlayer.setMoveSpeed($gamePlayer._moveSpeed);
 };
 
 const KRD_Game_Party_removeActor = Game_Party.prototype.removeActor;
 Game_Party.prototype.removeActor = function(actorId) {
 	KRD_Game_Party_removeActor.apply(this, arguments);
-	$gamePlayer.setMoveSpeed(DEFAULT_WALK_SPEED);
+	$gamePlayer.setMoveSpeed($gamePlayer._moveSpeed);
+};
+
+const KRD_Game_Actor_changeClass = Game_Actor.prototype.changeClass;
+Game_Actor.prototype.changeClass = function(classId, keepExp) {
+	KRD_Game_Actor_changeClass.apply(this, arguments);
+	$gamePlayer.setMoveSpeed($gamePlayer._moveSpeed);
 };
 
 //--------------------------------------

ebe8c2d5bd628709c53642a3043a0d13359b3d5b
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2023-06-30 07:32:42 +0900

    タグとパラメータ追加

--- KRD_MZ_StateMaxCount.js
+++ KRD_MZ_StateMaxCount.js
@@ -5,16 +5,28 @@
  * @url https://github.com/kuroudo119/RPGMZ-Plugin
  * @author kuroudo119 (くろうど)
  * 
- * @param GOOD_STATE
+ * @param PUSH_OUT_FLAG
+ * @text 押し出しフラグ
+ * @desc 上限を超える時に古いステートを解除して付与する: true ／ 付与できない: false
+ * @default false
+ * @type boolean
+ * 
+ * @param GOOD_STATE_MAX
  * @text 良性ステートの数
  * @desc 良いステートの付与上限数です。
  * @default 2
  * @type number
  * 
- * @param BAD_STATE
+ * @param BAD_STATE_MAX
  * @text 悪性ステートの数
  * @desc 悪いステートの付与上限数です。
- * @default 3
+ * @default 2
+ * @type number
+ * 
+ * @param OTHER_STATE_MAX
+ * @text その他ステートの数
+ * @desc その他ステートの付与上限数です。
+ * @default 1
  * @type number
  * 
  * @help
@@ -34,18 +46,21 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 ## 概要
 
 ステートの付与数に上限を設定します。
-上限を超えて付与される場合、古いステートが解除されます。
+
+上限を超えて付与される場合、押し出しフラグにより処理されます。
 
 ## 使い方
 
 ステートのメモ欄に <goodState> または <badState> と書いてください。
+または <otherState> も使えます。
 
 goodState は良性ステート、badState は悪性ステートとして、
+otherState はその他ステートとして、
 それぞれ数をカウントします。
 
 ## 補足
 
-タグ <goodState> <badState> を書いてないステートは
+タグ <goodState> <badState> <otherState> を書いてないステートは
 上限数に関係なく付与されます。
 
 ## 更新履歴
@@ -54,6 +69,7 @@ goodState は良性ステート、badState は悪性ステートとして、
 - ver.0.1.0 (2023/06/29) 非公開版完成
 - ver.0.2.0 (2023/06/29) ステート優先度に対応
 - ver.1.0.0 (2023/06/29) 公開
+- ver.1.1.0 (2023/06/30) タグotherState追加、押し出しフラグ追加
 
  * 
  * 
@@ -66,23 +82,37 @@ goodState は良性ステート、badState は悪性ステートとして、
 const PLUGIN_NAME = document.currentScript.src.match(/^.*\/(.*).js$/)[1];
 const PARAM = PluginManager.parameters(PLUGIN_NAME);
 
-const GOOD_STATE = Number(PARAM["GOOD_STATE"]) || 0;
-const BAD_STATE = Number(PARAM["BAD_STATE"]) || 0;
+const GOOD_STATE_MAX = Number(PARAM["GOOD_STATE_MAX"]) || 0;
+const BAD_STATE_MAX = Number(PARAM["BAD_STATE_MAX"]) || 0;
+const OTHER_STATE_MAX = Number(PARAM["OTHER_STATE_MAX"]) || 0;
 
 const GOOD_STATE_TAG = "goodState";
 const BAD_STATE_TAG = "badState";
+const OTHER_STATE_TAG = "otherState";
+
+const STATE_DATA_LIST = [
+	{tag: GOOD_STATE_TAG, max: GOOD_STATE_MAX},
+	{tag: BAD_STATE_TAG, max: BAD_STATE_MAX},
+	{tag: OTHER_STATE_TAG, max: OTHER_STATE_MAX},
+];
+
+const PUSH_OUT_FLAG = PARAM["PUSH_OUT_FLAG"] === "true";
 
 //--------------------------------------
+// ステート追加処理
 
 const KRD_Game_Battler_addState = Game_Battler.prototype.addState;
 Game_Battler.prototype.addState = function(stateId) {
-	const goodState = $dataStates[stateId].meta[GOOD_STATE_TAG];
-	const badState = $dataStates[stateId].meta[BAD_STATE_TAG];
-	if (goodState) {
-		this.addStateByCount(stateId, GOOD_STATE_TAG, GOOD_STATE);
-	} else if (badState) {
-		this.addStateByCount(stateId, BAD_STATE_TAG, BAD_STATE);
-	} else {
+	let added = false;
+	for (const data of STATE_DATA_LIST) {
+		if ($dataStates[stateId].meta[data.tag]) {
+			this.addStateByCount(stateId, data.tag, data.max);
+			added = true;
+			break;
+		}
+	}
+
+	if (!added) {
 		KRD_Game_Battler_addState.apply(this, arguments);
 	}
 };
@@ -92,19 +122,24 @@ Game_Battler.prototype.addStateByCount = function(stateId, tag, max) {
 	if (states.length < max) {
 		KRD_Game_Battler_addState.call(this, stateId);
 	} else {
-		if (this.isStateAddable(stateId)) {
-			if (!this.isStateAffected(stateId)) {
-				const erased = states.shift();
-				this.eraseState(erased);
-
-				this.addNewState(stateId);
-				this.refresh();
+		if (PUSH_OUT_FLAG) {
+			if (this.isStateAddable(stateId)) {
+				if (!this.isStateAffected(stateId)) {
+					const erased = states.shift();
+					this.eraseState(erased);
+	
+					this.addNewState(stateId);
+					this.refresh();
+				}
+				this.resetStateCounts(stateId);
+				this._result.pushAddedState(stateId);
 			}
-			this.resetStateCounts(stateId);
-			this._result.pushAddedState(stateId);
 		}
 	}
 };
+
+//--------------------------------------
+// ステート優先度の処理
 
 // 上書き
 Game_BattlerBase.prototype.sortStates = function(states) {

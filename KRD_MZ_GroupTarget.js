c31e68b2973531d4935a186da029d69fc0f40e9a
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2024-07-12 22:34:38 +0900

    最新化

--- KRD_MZ_GroupTarget.js
+++ KRD_MZ_GroupTarget.js
@@ -22,11 +22,12 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 ## 仕様
 
 アクターが使用したスキル（アイテム）の
-「範囲が単体」かつ「連続回数が9」の時、
+「範囲が単体」または「範囲がランダム1体」
+かつ、メモ欄に <GroupTarget> がある時、
 同じ敵番号をグループとして対象範囲にします。
 
 敵キャラがそのスキル使用した場合、
-連続回数を1とします。
+グループになりません。
 
 尚、対象が既に倒されている場合、
 index 0 から順に生存チェックして、
@@ -37,6 +38,7 @@ index 0 から順に生存チェックして、
 - ver.0.0.1 (2023/05/27) 作成開始
 - ver.0.1.0 (2023/05/27) 非公開版完成
 - ver.1.0.0 (2023/05/27) 公開
+- ver.2.0.0 (2023/10/19) metaタグ版に変更
 
  * 
  * 
@@ -46,15 +48,14 @@ index 0 から順に生存チェックして、
 
 "use strict";
 
-const GROUP_SCOPE = 1;
-const GROUP_REPEATS = 9;
-
-const ENEMY_USE_REPEATS = 1;
+const GROUP_SCOPE = [1, 3];
 const TROOP_MAX_MEMBERS = 8;
+
+const TAG_GROUP_TARGET = "GroupTarget";
 
 //--------------------------------------
 
-const KRD_Game_Action_makeTargets = Game_Action.prototype.makeTargets;
+const _Game_Action_makeTargets = Game_Action.prototype.makeTargets;
 Game_Action.prototype.makeTargets = function() {
 	if (this.isForEnemyGroup() && this.subject().isActor()) {
 			const targets = [];
@@ -65,34 +66,20 @@ Game_Action.prototype.makeTargets = function() {
 			}
 			return targets;
 	} else {
-		return KRD_Game_Action_makeTargets.apply(this, arguments);
+		return _Game_Action_makeTargets.call(this, ...arguments);
 	}
+};
+
+Game_Action.prototype.isForEnemyGroup = function() {
+	return GROUP_SCOPE.includes(this.item().scope) && !!this.item().meta[TAG_GROUP_TARGET];
 };
 
 Game_Action.prototype.targetsForGroup = function() {
 	const unit = this.opponentsUnit();
 	const aliveMembers = unit.aliveMembers();
-	const targets = this.setEnemyGroup(aliveMembers);
+	const enemyId = this.isForRandom() ? unit.randomTarget().enemyId() : $gameTroop._enemies[this._targetIndex].enemyId();
+	const targets = aliveMembers.filter(target => target.enemyId() === enemyId);
 	return targets;
-};
-
-Game_Action.prototype.isForEnemyGroup = function() {
-	return this.item().scope === GROUP_SCOPE && this.item().repeats === GROUP_REPEATS;
-};
-
-Game_Action.prototype.setEnemyGroup = function(targets) {
-	const enemyId = $gameTroop._enemies[this._targetIndex].enemyId();
-	const group = targets.filter(target => target.enemyId() === enemyId);
-	return group;
-};
-
-const KRD_Game_Action_numRepeats = Game_Action.prototype.numRepeats;
-Game_Action.prototype.numRepeats = function() {
-	if (this.item().repeats === GROUP_REPEATS) {
-		return ENEMY_USE_REPEATS;
-	} else {
-		return KRD_Game_Action_numRepeats.apply(this, arguments);
-	}
 };
 
 //--------------------------------------

31d57a721cfd0ac76bf0db20b1d7d422a9dce5c6
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2024-09-22 14:47:26 +0900

    機能追加した

--- KRD_MZ_MinDamage.js
+++ KRD_MZ_MinDamage.js
@@ -7,14 +7,26 @@
  * 
  * @param MIN_DAMAGE
  * @text 最低ダメージ
- * @desc ダメージがこの値より小さい時にこの値になります。
+ * @desc ダメージがこの値より小さい時にこの値になります。初期値 2。
  * @default 2
  * @type number
  * 
  * @param MIN_PERCENT
  * @text 最低ダメージ確率
- * @desc ダメージが最低ダメージより小さい時にこの確率（パーセント）で最低ダメージになります。
+ * @desc ダメージが最低ダメージより小さい時にこの確率（パーセント）で最低ダメージになります。初期値 50。
  * @default 50
+ * @type number
+ * 
+ * @param MIN_DAMAGE_PARAM
+ * @text 最低ダメージ能力値
+ * @desc ダメージがこの能力値より小さい時にこの値になります。能力値の番号（0～7）。初期値 2（攻撃力）。
+ * @default 2
+ * @type number
+ * 
+ * @param MIN_DAMAGE_PARAM_RATE
+ * @text 最低ダメージ能力値割合
+ * @desc 「最低ダメージ能力値」とする割合（パーセント）です。初期値 10。
+ * @default 10
  * @type number
  * 
  * @help
@@ -36,6 +48,7 @@ https://github.com/kuroudo119/RPGMZ-Plugin/blob/master/LICENSE
 - ver.0.0.1 (2024/07/11) 作成開始
 - ver.0.1.0 (2024/07/11) 非公開版完成
 - ver.1.0.0 (2024/07/11) 公開
+- ver.1.1.0 (2024/09/22) 「最低ダメージ能力値」を追加
 
  * 
  * 
@@ -51,20 +64,37 @@ const PARAM = PluginManager.parameters(PLUGIN_NAME);
 const MIN_DAMAGE = Number(PARAM["MIN_DAMAGE"]) || 0;
 const MIN_PERCENT = Number(PARAM["MIN_PERCENT"]) || 0;
 
+const MIN_DAMAGE_PARAM = PARAM["MIN_DAMAGE_PARAM"] ? Number(PARAM["MIN_DAMAGE_PARAM"]) : NaN;
+const MIN_DAMAGE_PARAM_RATE = (Number(PARAM["MIN_DAMAGE_PARAM_RATE"]) || 0) / 100;
+
 //--------------------------------------
 
 const _Game_Action_evalDamageFormula = Game_Action.prototype.evalDamageFormula;
 Game_Action.prototype.evalDamageFormula = function(target) {
 	const base = _Game_Action_evalDamageFormula.call(this, ...arguments);
-	if (base >= 0 && base < MIN_DAMAGE) {
+	return this.checkMinDamage(base);
+};
+
+Game_Action.prototype.checkMinDamage = function(damage) {
+	const minDamage = this.minDamage();
+	if (damage >= 0 && damage < minDamage) {
 		const random = Math.floor(Math.random() * 100);
 		if (random < MIN_PERCENT) {
-			return MIN_DAMAGE;
+			return minDamage;
 		} else {
-			return base;
+			return damage;
 		}
 	} else {
-		return base;
+		return damage;
+	}
+};
+
+Game_Action.prototype.minDamage = function() {
+	if (isNaN(MIN_DAMAGE_PARAM)) {
+		return MIN_DAMAGE;
+	} else {
+		const damage = this.subject().param(MIN_DAMAGE_PARAM) * MIN_DAMAGE_PARAM_RATE;
+		return damage > MIN_DAMAGE ? damage : MIN_DAMAGE;
 	}
 };
 
